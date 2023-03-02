32db29064c48f7a3504969984265e230b7d3595d
garlic0x1 <garlic1@protonmail.com>
2023-03-02 04:05:45 -0600

    fc

---
+++ httpclient.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"sync"
+	"os"
+)
+
+var url = "http://localhost:8000"
+var key = os.Getenv("OVERCAST_API_KEY")
+
+func endpoint(path string) string {
+	return fmt.Sprintf("%s%s", url, path)
+}
+
+func authReq(method string, url string, contentType string, body io.Reader) (*http.Response, error) {
+log.Println(key)
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-type", contentType)
+	req.Header.Set("Authorization", fmt.Sprintf("Key %s", key))
+	resp, err := http.DefaultClient.Do(req)
+	return resp, err
+}
+
+func ApiGet[T any](path string, to T) (T, error) {
+	var res T
+	resp, err := authReq(
+		"GET",
+		endpoint(path),
+		"application/json",
+		nil,
+	)
+	if err != nil {
+		return res, err
+	}
+
+	log.Println(resp.Status)
+
+	err = json.NewDecoder(resp.Body).Decode(&res)
+	return res, err
+
+}
+
+func ApiPost[T any](path string, data any, to T) (T, error) {
+	var res T
+	bslc, err := json.Marshal(data)
+	if err != nil {
+		return res, err
+	}
+	resp, err := authReq(
+		"POST",
+		endpoint(path),
+		"application/json",
+		bytes.NewReader(bslc),
+	)
+	if err != nil {
+		return res, err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&res)
+	return res, err
+}
+
+func count(queryString string) (int, error) {
+	resp, err := ApiPost("/search/count", SearchQuery{Query: queryString}, CountResp{})
+	return resp.Count, err
+}
+
+func AssetOverview(domain string) error {
+	var resp map[string]any
+	resp, err := ApiGet(fmt.Sprintf("/overview/asset?domain=%s", domain), resp)
+	if err != nil {
+		return err
+	}
+	Output <- resp
+	return nil
+}
+
+func Search(page int, queryString string) error {
+	var searchPage = func(p int) error {
+		query := SearchQuery{Page: p, Query: queryString}
+		resp, err := ApiPost("/search", query, any(1))
+		if err != nil {
+			return err
+		}
+		Output <- resp
+		return nil
+	}
+
+	count, err := count(queryString)
+	if err != nil {
+		return err
+	}
+
+	pages := (count / 100) + 1
+	if page >= 0 {
+		if page > pages {
+			return fmt.Errorf("Last page is %d", pages)
+		}
+		return searchPage(page)
+	}
+
+	var wg sync.WaitGroup
+	for i := 1; i <= pages; i++ {
+		wg.Add(1)
+		go func(p int) {
+			defer wg.Done()
+			searchPage(p)
+		}(i)
+	}
+	wg.Wait()
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	//"sync/atomic"
+)
+
+var Output = make(chan any)
+
+type SearchQuery struct {
+	Page  int    `json:"page"`
+	Query string `json:"query"`
+}
+
+type CountResp struct {
+	Count int `json:"count"`
+}
+
+func writer() {
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+	for res := range Output {
+		err := json.NewEncoder(w).Encode(res)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func app() {
+	searchCmd := flag.NewFlagSet("search", flag.ExitOnError)
+	searchAll := flag.Bool("all", false, "get all pages")
+	searchPage := flag.Int("page", 1, "get page")
+
+	overviewCmd := flag.NewFlagSet("overview", flag.ExitOnError)
+	//overviewOrg := flag.Bool("org", false, "organization overview (replaces domain)")
+
+	switch os.Args[1] {
+	case "search":
+		searchCmd.Parse(os.Args[2:])
+		if *searchAll {
+			*searchPage = -1
+		}
+		err := Search(*searchPage, searchCmd.Args()[0])
+		if err != nil {
+			log.Println(err)
+		}
+	case "overview":
+		overviewCmd.Parse(os.Args[2:])
+		err := AssetOverview(overviewCmd.Args()[0])
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	close(Output)
+}
+
+func main() {
+	go app()
+	writer()
+}
